91b02aa26445669e009db22ad2d16a006dda89b1
zhujunxi <531578783@qq.com>
2019-05-27 17:19:32 +0800

    [build] eslint规则调整

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import ElementUI from "element-ui";
-Vue.config.productionTip = false;
-import "element-ui/lib/theme-chalk/index.css";
-Vue.use(ElementUI);
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+Vue.config.productionTip = false
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 new Vue({
     router,
     store,
     render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,4 @@ module.exports = {
             errors: true
         }
     }
-};
+}
